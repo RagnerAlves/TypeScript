cc0659befe62cc864df0c784dbe68c5e3a159d39
Ragner Alves <ragnerf@hotmail.com>
2024-02-16 10:31:21 -0300

    exe2_boolean

--- modulo-01/helloword/app.js
+++ modulo-01/helloword/app.js
@@ -1,10 +1,6 @@
-//let mensagem: string = "Hello Word !!! "
-//console.log(mensagem);
-var carro;
+"use strict";
+let mensagem = "Hello Word !!! ";
+console.log(mensagem);
+let carro;
 carro = { nome: 'Toyota Yaris Seda x60', ano: 2019, preco: 80000 };
 console.log(carro);
-// => Funciton
-function multiplicarNumero(num1, num2) {
-    return num1 * num2;
-}
-console.log(multiplicarNumero(5, 3));
--- modulo-01/helloword/app.ts
+++ modulo-01/helloword/app.ts
@@ -1,15 +1,6 @@
-//let mensagem: string = "Hello Word !!! "
-//console.log(mensagem);
+let mensagem: string = "Hello Word !!! "
+console.log(mensagem);
 
-let carro
-carro = {nome: 'Toyota Yaris Seda x60', ano: 2019, preco: 80000}
-console.log(carro)
-
-// => Funciton
-
-function multiplicarNumero(num1: number, num2: number)
-{
-    return (num1 * num2)
-}
-
-console.log(multiplicarNumero(5,3))
+let carro;
+carro = {nome: 'Toyota Yaris Seda x60', ano: 2019, preco: 80000};
+console.log(carro);
